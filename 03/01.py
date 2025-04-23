f4e3b3f4e30f77e99c58117093f279eccf942ee3
Aditya22007 <24bit123@sot.pdpu.ac.in>
2025-04-23 20:16:30 +0530

    Add files via upload

---
+++ 03/01.py
@@ -0,0 +1,14 @@
+# 24bit123
+
+# Que-1
+
+s = input("Enter a string: ").lower()
+
+vowels = "aeiou"
+
+no_of_vowels = 0
+
+for i in vowels:
+    no_of_vowels += s.count(i)
+
+print("Number of vowels: ", no_of_vowels)
