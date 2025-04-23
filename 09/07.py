f370825efb3e1d504e646c57706ffaa7594e4f66
Aditya22007 <24bit123@sot.pdpu.ac.in>
2025-04-23 20:23:29 +0530

    Add files via upload

---
+++ 09/07.py
@@ -0,0 +1,7 @@
+# 24bit123
+
+# Que-7
+def ispalindrome(s):
+    return s == s[::-1]
+
+print(ispalindrome(input("Enter string: ")))
---
+++ 09/08.py
@@ -0,0 +1,10 @@
+# 24bit123
+
+# Que-8
+def convert():
+    s = input("Enter string here: ")
+    l = s.split()
+    l = list(set(l))
+    l.sort()
+    print(l)
+convert()
